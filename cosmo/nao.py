5a30653e5cb33b509b5b5fbaf605b3281c7847ea
Naonori Sugiyama <nao.s.sugiyama@gmail.com>
2023-12-19 22:50:31 +0900

    Add files via upload

---
+++ cosmo/nao.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os
+import readline
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+def cosmology():
+    print("daisuki")
+    print("honto?")
+
