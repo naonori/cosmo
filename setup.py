a38371ed03d406d44114e7ff15ba8d1b29687aaf
Naonori Sugiyama <nao.s.sugiyama@gmail.com>
2023-12-19 22:50:43 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os
+import readline
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+from setuptools import setup, find_packages
+
+setup(
+    name="cosmo",
+    version="0.1",
+    packages=find_packages()
+)
+
+
+
+
